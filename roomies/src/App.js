b84046b0cca3b3a26efde19650d410d96e099547
DevinAnga <DevinAnga.dev@Gmail.com>
2020-02-08 18:47:34 -0500

    added a generic textarea component

--- roomies/src/App.js
+++ roomies/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import GenericButton from './GenericButton';
 import Nav from './Nav';
 import Profile from './Profile';
+import TextBox from './TextBox';
 
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
             <div className = "App" >
                 <Nav/>
                 <Profile/>
-                <GenericButton name = "woof" buttonClass="button red"/>
+                <GenericButton name="woof" buttonClass="button red"/>
+                <TextBox height="200px" width="500px"  name="About me!"/>
             </div>
         );
     }
--- roomies/src/Nav.js
+++ roomies/src/Nav.js
@@ -4,24 +4,24 @@ import React, { Component } from 'react';
 class Nav extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 More
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Messages</a>
-                <a class="dropdown-item" href="#">Balance</a>
-                <a class="dropdown-item" href="#">Profile</a>
-                <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Logout</a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">Messages</a>
+                <a className="dropdown-item" href="#">Balance</a>
+                <a className="dropdown-item" href="#">Profile</a>
+                <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">Logout</a>
               </div>
             </li>
           </ul>
