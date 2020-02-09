2066ab07b53ebf3f474f4f95dbcf256fbf7338d9
nbraden <n_braden@fanshaweonline.ca>
2020-02-08 19:23:30 -0500

    723

--- roomies/src/App.js
+++ roomies/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 // import './main.module.css'
 import GenericButton from './GenericButton';
-// import Nav from './Nav';
+import Nav from './Nav';
 import Profile from './Profile';
+import 'bootstrap/dist/css/bootstrap.css';
+import Example from './Nav';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 
 class App extends Component {
     render() {
         return ( 
             <div className = "App" >
-                {/* <Nav/> */}
+                <Example/>
                 <Profile/>
                 <GenericButton name = "woof" buttonClass="button red"/>
             </div>
--- roomies/src/Nav.js
+++ roomies/src/Nav.js
@@ -1,34 +1,60 @@
-import React, { Component } from 'react';
-// import styles from './main.module.css'; 
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
-class Nav extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                More
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Messages</a>
-                <a class="dropdown-item" href="#">Balance</a>
-                <a class="dropdown-item" href="#">Profile</a>
-                <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Logout</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Nav;
+export default Example;
