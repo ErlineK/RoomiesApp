bd44b3d38a5d8a2215b846a6b2be27ae9ed74e99
Erline Katz <e_katz@fanshaweonline.ca>
2020-04-09 16:47:47 -0400

    backend init

---
+++ roomies/backend/Models/Chore.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ChoreSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true
+  },
+  leader: {
+    type: String,
+    required: true
+  },
+  dueDate: {
+    type: Date,
+    required: false
+  },
+  complete: {
+    type: Boolean,
+    default: true,
+    required: false
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Chore = mongoose.model("chore", ChoreSchema);
